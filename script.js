d204ec3aea7a3fef7a09aaabb8b208a6e136f5bf
Jeff Beers <jeffb92@outlook.com>
2022-07-18 14:53:55 -0400

    mobile menu adjustments. comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function onClickMenu(){
-    document.getElementById("menu").classList.toggle("change"); // Hamburger to X //
-    document.getElementById("nav").classList.toggle("change"); // Hamburger Links //
-    document.getElementById("menu-bg").classList.toggle("change-bg"); // Hamburger BG //
+    document.getElementById("menu").classList.toggle("change"); // Hamburger to X. toggle changes class when clicked//
+    document.getElementById("nav").classList.toggle("change"); // Hamburger Links appear on click//
+    document.getElementById("menu-bg").classList.toggle("change-bg"); // Hamburger Background appears on click//
 }
