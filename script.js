8ad1a6817c1dc4e4a96262eb3ce93f31f7d9afe9
Jeff Beers <jeffb92@outlook.com>
2022-07-18 14:13:49 -0400

    mobile hamburger menu with animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Hamburger to X //
 function onClickMenu(){
-    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("menu").classList.toggle("change"); // Hamburger to X //
+    document.getElementById("nav").classList.toggle("change"); // Hamburger Links //
+    document.getElementById("menu-bg").classList.toggle("change-bg"); // Hamburger BG //
 }
