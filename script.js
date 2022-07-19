850e1050aaba098163d1801ebe500e553c9adaaf
Jeff Beers <jeffb92@outlook.com>
2022-07-19 14:46:15 -0400

    changed colors, updated contact form

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ function onClickMenu(){
     document.getElementById("nav").classList.toggle("change"); // Hamburger Links appear on click//
     document.getElementById("menu-bg").classList.toggle("change-bg"); // Hamburger Background appears on click//
 }
+
